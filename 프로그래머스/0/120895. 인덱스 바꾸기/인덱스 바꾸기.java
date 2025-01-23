ea09d72fb14f50ebcccee53131d685f8eeff367b
mallang <sunyeon4012@gmail.com>
2025-01-23 18:13:09 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] charArr = my_string.toCharArray();
+        
+        charArr[num1] = my_string.charAt(num2);
+        charArr[num2] = my_string.charAt(num1);
+        
+        String answer = String.valueOf(charArr);
+        
+        return answer;
+    }
+}
