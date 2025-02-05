9842b277a355ffb0cf80c80d6f347ffd0b9ac01b
mallang <sunyeon4012@gmail.com>
2025-02-06 00:00:14 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -15,3 +15,17 @@ class Solution {
         return answer;
     }
 }
+
+/*
+public int solution(int[] date1, int[] date2) {
+
+        LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+
+        if (dateA.isBefore(dateB)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+*/
