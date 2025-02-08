de16b2f5446ca7ad102507dcf9ec48b2e9cff562
mallang <sunyeon4012@gmail.com>
2025-02-08 12:04:26 +0900

    [level 0] Title: 배열 만들기 5, Time: 6.99 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < intStrs.length; i++) {
+            int num = Integer.parseInt(intStrs[i].substring(s, s+l));
+            
+            if (num > k) {
+                list.add(num);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
